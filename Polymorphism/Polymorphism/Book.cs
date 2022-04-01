ab07aa82a8da5c5e6836f74ab80f4fa6b604e89b
Emjliy <6p304s8@code.edu.az>
2022-04-01 09:25:24 +0400

    added

---
+++ Polymorphism/Polymorphism/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polymorphism
+{
+    class Book:Product
+    {
+        public string Genre { get; set; }
+        public Book(int no,string name,double price,string genre):base(no,name,price)
+        {
+            Genre = genre;
+        }
+    }
+}
---
+++ Polymorphism/Polymorphism/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kitab sayi:");
+            int size = int.Parse(Console.ReadLine());
+            Book[] book = new Book[size];
+            for (int i = 0; i < size; i++)
+            {
+                Book[] books = new Book[size];
+                books[i].No = Convert.ToInt32(Console.ReadLine());
+                books[i].Name = Console.ReadLine();
+                books[i].Price = Convert.ToDouble(Console.ReadLine());
+                books[i].Genre = Console.ReadLine();
+            }
+            Console.WriteLine("1.Filter");
+            Console.WriteLine("2.Show all books");
+            Console.WriteLine("3.Close the program");
+            int input = Convert.ToInt32(Console.ReadLine());
+            if (input == 1)
+            {
+                int minPrice = Convert.ToInt32(Console.ReadLine());
+                int maxPrice = Convert.ToInt32(Console.ReadLine());
+                Array.Sort(book);
+                Book[] sort = new Book[book.Length];
+                Console.WriteLine(sort);
+
+            }
+            else if (input == 2)
+            {
+                Console.WriteLine(book);
+            }
+            else
+            {
+                Console.WriteLine();
+            }
+        }
+
+
+    }
+}
